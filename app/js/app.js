cf760b011040c27409fd46143ef52aae50c9cf3c
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-05-06 21:48:35 +0200

    #Clean up - deleted a bunch of files from the seed project - upgraded to the latest version of AngularJS - refactored stuff to be in line with Angulars "module" concept

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,17 @@
 'use strict';
+/*
+
+var App = angular
+            .module('myApp',[])
+            .config(['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/stream', {template: 'partials/streamview.html', controller: 'StreamController'});
+            $routeProvider.otherwise({redirectTo: '/stream'});
+          }]);
+*/
 
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/stream', {template: 'partials/streamview.html', controller: StreamController});
-    $routeProvider.otherwise({redirectTo: '/stream'});
-  }]);
+var App = angular
+    .module('App',[], function($routeProvider){
+        $routeProvider.when('/stream', {template: 'partials/streamview.html', controller: 'StreamController'});
+        $routeProvider.otherwise({redirectTo: '/stream'});
+    });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,47 +1,46 @@
 'use strict';
 /* App Controllers */
 
-
-function StreamController($scope, $browser) {
+App.controller('StreamController',['$scope',function ($scope) {
 
     $scope.stream = [];
 
-    //TODO figure out why the binding doesnt work in the traditional way
+    //TODO figure out why the binding doesn't work in the traditional way
     $scope.refreshData = function(username){
         $.ajaxAsObservable({
             url: "https://api.github.com/users/" + username + "/received_events",
             dataType: "jsonp"
         })
-        .select(function(payload){
-            return payload.data.data;
-        })
-        .subscribe(function(data){
-            if (data.message){
-                $scope.$apply(function(){
-                    $scope.stream = [];
-                    $scope.projects = [];
-                });
-            }
-            else{
-                $scope.$apply(function(){
-                    $scope.stream = data;
+            .select(function(payload){
+                return payload.data.data;
+            })
+            .subscribe(function(data){
+                if (data.message){
+                    $scope.$apply(function(){
+                        $scope.stream = [];
+                        $scope.projects = [];
+                    });
+                }
+                else{
+                    $scope.$apply(function(){
+                        $scope.stream = data;
 
-                    $scope.projects =  _.chain(data)
-                        .groupBy(function(item){
-                            return item.repo.name;
-                        })
-                        .reduce(function(acc, groupedValues){
-                            var name = groupedValues[0].repo.name;
-                            acc[name] = {
-                                name: name,
-                                show: true
-                            };
-                            return acc;
-                        }, {})
-                        .value();
-                });
-            }
-        });
+                        $scope.projects =  _.chain(data)
+                            .groupBy(function(item){
+                                return item.repo.name;
+                            })
+                            .reduce(function(acc, groupedValues){
+                                var name = groupedValues[0].repo.name;
+                                acc[name] = {
+                                    name: name,
+                                    show: true
+                                };
+                                return acc;
+                            }, {})
+                            .value();
+                    });
+                }
+            });
     };
 
     $scope.doFoo = function(){
@@ -62,11 +61,4 @@ function StreamController($scope, $browser) {
         $scope.showFilter = !$scope.showFilter;
     };
 
-}
-StreamController.$inject = ['$scope', '$browser'];
-
-
-function MyCtrl2() {
-    this.foo = "blubb";
-}
-MyCtrl2.$inject = [];
+}]);
