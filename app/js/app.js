cd4ccb93255cb9d2164236e84de742ab0a25aaa8
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-06-04 22:37:45 +0200

    changed routing for fuck phonegap sake

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,6 @@
 var App = angular
             .module('App',[])
             .config(['$routeProvider', function($routeProvider) {
-            $routeProvider.when('/stream', {template: 'partials/streamview.html', controller: 'StreamController'});
+            $routeProvider.when('/stream', {template: 'streamview.html', controller: 'StreamController'});
             $routeProvider.otherwise({redirectTo: '/stream'});
           }]);
